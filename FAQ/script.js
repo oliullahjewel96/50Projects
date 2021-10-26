4b508c8e64a93460b63bcb4f7fd9f10020742aaf
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-10-26 12:11:00 +0600

    Added js functionality and a little bit of CSS

---
+++ FAQ/script.js
@@ -0,0 +1,7 @@
+const faqToggle = document.querySelectorAll(".faq-toggle");
+
+faqToggle.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+        toggle.parentNode.classList.toggle("active");
+    });
+});
